ddd8365252bf0768abacaab605bf465c0b0d8b4a
Damian <justice.damian96@gmail.com>
2021-07-21 05:34:01 +1000

    Assignment Complete

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+let test = ['john', 'jenny', 'jimmy'];
+function writeCards(cardsNames, event) {
+    let newArray = [];
+    for (let i = 0; i < cardsNames.length; i++) {
+        let message = `Thank you, ${cardsNames[i]}, for the wonderful ${event} gift!`
+        console.log(message);
+        newArray.push(message);
+        
+    }
+    return newArray;
+}
+
+
+
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num);
+        num--;
+    }
+}
